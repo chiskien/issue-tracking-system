eb7df658bb256476158a59eadcab8c0cc1065504
ChisKien <chiskien03214@gmail.com>
2021-11-01 10:48:30 +0700

    hihi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {ClarityModule} from '@clr/angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { IssueListComponent } from './issue-list/issue-list.component';
+import {IssueListComponent} from './issue-list/issue-list.component';
 
 @NgModule({
   declarations: [
