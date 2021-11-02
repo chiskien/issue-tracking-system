6aaa42c6b8b88a81f6743507eb12085b25c1c016
ChisKien <chiskien03214@gmail.com>
2021-11-02 15:59:52 +0700

    Create formBuilder and FormGroup with four properties

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-issue-report',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-report.component.scss']
 })
 export class IssueReportComponent implements OnInit {
+  formGroup: FormGroup | undefined;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  private createForm() {
+    this.formGroup = this.formBuilder.group({
+      title: [''],
+      description: [''],
+      priority: [''],
+      type: ['']
+    })
+  }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
 }
